18a7034cf01a08d14fc08119ad23a88ed255fc08
Sammy-T <cyam9019@aim.com>
2022-10-05 20:14:59 -0400

    Utilize Monaco's find and replace
    Replace custom find and replace implementations with Monaco's built-in ones.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,14 @@
 import {editor} from './editor/init';
-import {Environment, EventsOn, WindowSetDarkTheme} from '../wailsjs/runtime/runtime';
+import {Environment, EventsOn} from '../wailsjs/runtime/runtime';
 import {NewFile, OpenFile, SaveAs, Save} from '../wailsjs/go/main/App';
 
 let platform;
-let restoreText; // For storing/restoring text while transitioning between header focus
-const restoreSelection = {start: -1, end: -1};
 
 const menuDropdowns = document.querySelectorAll('#menu details');
 const menuItems = document.querySelectorAll('#menu a');
 const modals = document.querySelectorAll('dialog');
-const header = document.querySelector('body > header');
-const textArea = document.querySelector('#text-input'); //// TODO: Remove
 const saveStatus = document.querySelector('#save-status');
 const lineCount = document.querySelector('#line-count');
-
-const templateFind = document.querySelector('#template-find');
-const templateReplace = document.querySelector('#template-replace');
 
 const modalOptions = document.querySelector('#modal-options');
 
@@ -83,182 +76,26 @@ function onKey(event) {
             }
             break;
         case 'f':
-            toggleFind();
+            triggerFind();
             break;
         case 'h':
-            toggleReplace();
+            triggerReplace();
             break;
     }
 }
 
 /**
- * A helper to make a copy of the text and selection when the header gains focus.
+ * Triggers monaco editor's 'replace' action.
  */
-function onHeaderFocusIn() {
-    restoreText = textArea.value;
-    restoreSelection.start = textArea.selectionStart;
-    restoreSelection.end = textArea.selectionEnd;
-};
-
-/**
- * A helper to restore the text area's content using the stored copy when the header loses focus.
- */
-function onHeaderFocusOut() {
-    textArea.value = restoreText;
-    textArea.selectionStart = restoreSelection.start;
-    textArea.selectionEnd = restoreSelection.end;
-
-    restoreText = '';
-    restoreSelection.start = -1;
-    restoreSelection.end = -1;
-};
-
-/**
- * Searches for matching text starting from the current selection position.
- * @param {Event} event 
- */
-function onFind(event) {
-    event.preventDefault();
-    
-    const formData = new FormData(event.target);
-    const searchTerm = formData.get('search-term');
-    
-    // Get the selection/cursor position
-    const selectionIndex = (textArea.selectionEnd === textArea.textLength) ? 0 : textArea.selectionEnd;
-
-    // Create a copy of the text starting from the selection position
-    const searchText = restoreText.substring(selectionIndex);
-
-    if(searchTerm === '' || searchText === '') return;
-
-    const searchIndex = searchText.indexOf(searchTerm); // Search for the text
-    
-    // If the text wasn't found, reset the textarea and selection
-    if(searchIndex < 0) {
-        textArea.value = restoreText;
-        textArea.selectionStart = 0;
-        textArea.selectionEnd = 0;
-        return;
-    }
-
-    // Determine the index as it would be in the original text
-    const matchIndex = searchIndex + selectionIndex;
-
-    // Display brackets around the matched text
-    textArea.value = restoreText.slice(0, matchIndex) + `[${searchTerm}]` 
-        + restoreText.slice(matchIndex + searchTerm.length);
-    
-    // Move the selection while including the bracket-enclosed match
-    textArea.selectionStart = matchIndex;
-    textArea.selectionEnd = matchIndex + searchTerm.length + 2;
-
-    // Store where the selection would be in the original text
-    restoreSelection.start = matchIndex;
-    restoreSelection.end = matchIndex + searchTerm.length;
+function triggerReplace() {
+    editor.trigger(null, 'editor.action.startFindReplaceAction');
 }
 
 /**
- * Replaces the currently selected text.
- * @param {Event} event 
+ * Triggers monaco editor's 'find' action.
  */
-function onReplace(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const searchTerm = formData.get('search-term');
-    const replaceTerm = formData.get('replace-term');
-
-    // Determine the currently selected text
-    const selectedTerm = restoreText.slice(restoreSelection.start, restoreSelection.end);
-
-    if(searchTerm === '') return;
-
-    // Trigger a search if the selection doesn't encompass any text
-    // or if the selected term doesn't match the search term.
-    if(restoreSelection.start === restoreSelection.end || selectedTerm !== searchTerm) {
-        console.warn(`Term not selected. replace: '${replaceTerm}', selected: '${selectedTerm}'`);
-        onFind(event);
-        return;
-    }
-
-    // Build the replacement text and set the new selection position
-    const replaceText = restoreText.slice(0, restoreSelection.start) + replaceTerm 
-        + restoreText.slice(restoreSelection.end);
-    
-    const selectionIndex = restoreSelection.start + replaceTerm.length;
-    
-    restoreText = replaceText;
-    textArea.value = replaceText;
-
-    textArea.selectionStart = selectionIndex;
-    textArea.selectionEnd = selectionIndex;
-
-    restoreSelection.start = selectionIndex;
-    restoreSelection.end = selectionIndex;
-}
-
-/**
- * Replaces the current header or toggles the 'replace' header.
- */
-function toggleReplace() {
-    if(checkHeaderContent('replace')) return; // Toggle or replace the header content
-
-    // A helper to route to the appropriate function based on the event's submitter
-    const routeMethod = function(event) {
-        switch(event.submitter.name) {
-            case 'find-button':
-                onFind(event);
-                break;
-            case 'replace-button':
-                onReplace(event);
-                break;
-        }
-    }
-
-    // Create the form from the template
-    const replaceForm = templateReplace.content.firstElementChild.cloneNode(true);
-
-    replaceForm.addEventListener('focusin', onHeaderFocusIn);
-    replaceForm.addEventListener('focusout', onHeaderFocusOut);
-    replaceForm.addEventListener('submit', routeMethod);
-
-    header.appendChild(replaceForm); // Add the form to the header
-
-    replaceForm.querySelector('input').focus();
-}
-
-/**
- * Replaces the current header or toggles the 'find' header.
- */
-function toggleFind() {
-    if(checkHeaderContent('find')) return; // Toggle or replace the header content
-
-    // Create the form from the template
-    const findForm = templateFind.content.firstElementChild.cloneNode(true);
-
-    // findForm.addEventListener('focusin', onHeaderFocusIn);
-    // findForm.addEventListener('focusout', onHeaderFocusOut);
-    findForm.addEventListener('submit', onFind);
-
-    header.appendChild(findForm); // Add the form to the header
-
-    findForm.querySelector('input').focus();
-}
-
-/**
- * Checks for and removes header content. 
- * @param {String} elementId - The id to search for.
- * @returns {Boolean} - Whether the found content matches the passed element id.
- */
-function checkHeaderContent(elementId) {
-    let headerChild = header.firstElementChild;
-    
-    if(headerChild) {
-        header.innerHTML = ''; // Remove the content
-        textArea.focus();
-    }
-    
-    return headerChild?.id === elementId;
+function triggerFind() {
+    editor.trigger(null, 'actions.find');
 }
 
 /**
@@ -370,10 +207,10 @@ function onMenuItemClick(item) {
             SaveAs(editor.getValue());
             break;
         case 'find-term':
-            toggleFind();
+            triggerFind();
             break;
         case 'replace-term':
-            toggleReplace();
+            triggerReplace();
             break;
         case 'options':
             showOptions();
@@ -431,3 +268,5 @@ menuItems.forEach(initMenuItem);
 document.addEventListener('keydown', onKey);
 
 modals.forEach(initModal);
+
+// console.log(editor.getSupportedActions());
