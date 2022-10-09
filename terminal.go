579981bdbcfe2af82c302c544cfad604cf55ccbf
Sammy-T <cyam9019@aim.com>
2022-10-09 08:52:48 -0400

    Add gnome-terminal mapping
    This is untested and might not work.

--- terminal.go
+++ terminal.go
@@ -17,30 +17,28 @@ func getTerminals() map[string]Terminal {
 
 	switch runtime.GOOS {
 	case "windows":
-		cmdRoot := "cmd"
+		const cmdRoot = "cmd"
 
 		// Set Command Prompt terminal
 		terminals["Command Prompt"] = Terminal{"Command Prompt", cmdRoot, []string{"/C", "start"}}
 
 		// Set Git Bash terminal if the file is found
 		if bashPath := "C:/Program Files/Git/bin/bash.exe"; fileExists(bashPath) {
-			bash := Terminal{
-				Name:    "Git Bash",
-				CmdRoot: cmdRoot,
-				OpenCmd: []string{"/C", "start", "", bashPath},
-			}
+			bash := Terminal{"Git Bash", cmdRoot, []string{"/C", "start", "", bashPath}}
 
 			terminals[bash.Name] = bash
 		}
 	case "linux":
 		if qTerminalPath := "/usr/bin/qterminal"; fileExists(qTerminalPath) {
-			qTerminal := Terminal{
-				Name:    "qTerminal",
-				CmdRoot: "qterminal",
-				OpenCmd: []string{"qterminal"},
-			}
+			qTerminal := Terminal{"qTerminal", "qterminal", []string{"qterminal"}}
 
-			terminals["qTerminal"] = qTerminal
+			terminals[qTerminal.Name] = qTerminal
+		}
+
+		if gnomePath := "/usr/bin/gnome-terminal"; fileExists(gnomePath) {
+			gnomeTerminal := Terminal{"gnome-terminal", "gnome-terminal", []string{"gnome-terminal"}}
+
+			terminals[gnomeTerminal.Name] = gnomeTerminal
 		}
 	default:
 		log.Printf("OS %v not implemented\n", runtime.GOOS)
