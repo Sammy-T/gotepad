265f84d871ea83eca681e6b3b9b21d6086e18799
Sammy-T <cyam9019@aim.com>
2022-10-10 02:43:03 -0400

    Change default file name on file open

--- app.go
+++ app.go
@@ -12,14 +12,15 @@ import (
 
 // App struct
 type App struct {
-	ctx         context.Context
-	filePath    string
-	fileFilters []runtime.FileFilter
+	ctx             context.Context
+	defaultFilename string
+	filePath        string
+	fileFilters     []runtime.FileFilter
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	return &App{defaultFilename: "*.txt"}
 }
 
 // startup is called when the app starts. The context is saved
@@ -64,6 +65,7 @@ func (a *App) onLanguagesLoaded(optionalData ...interface{}) {
 // NewFile clears the file path, sets the window title, and emits an 'onNewFile' event
 func (a *App) NewFile() {
 	a.filePath = ""
+	a.defaultFilename = "*.txt"
 	runtime.WindowSetTitle(a.ctx, "gotepad")
 	runtime.EventsEmit(a.ctx, "onNewFile")
 }
@@ -84,6 +86,7 @@ func (a *App) OpenFile() {
 	}
 
 	a.filePath = filePath
+	a.defaultFilename = "*" + filepath.Ext(filePath)
 	runtime.WindowSetTitle(a.ctx, "gotepad - "+filepath.Base(filePath))
 
 	// Read the file at the selected file path
@@ -100,7 +103,7 @@ func (a *App) OpenFile() {
 // SaveAs opens a file dialog and saves the contents at the selected path
 func (a *App) SaveAs(contents string) {
 	options := runtime.SaveDialogOptions{
-		DefaultFilename: "*.txt",
+		DefaultFilename: a.defaultFilename,
 		Filters:         a.fileFilters,
 	}
 
